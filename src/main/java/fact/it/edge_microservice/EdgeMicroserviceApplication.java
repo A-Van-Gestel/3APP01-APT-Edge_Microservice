accbfd32a4b3fb2948de5931bc5be358402c1e22
Axel Van Gestel <r0784084@student.thomasmore.be>
2021-10-08 11:13:07 +0200

    Initial Java project

---
+++ src/main/java/fact/it/edge_microservice/EdgeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.edge_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeMicroserviceApplication.class, args);
+    }
+
+}
